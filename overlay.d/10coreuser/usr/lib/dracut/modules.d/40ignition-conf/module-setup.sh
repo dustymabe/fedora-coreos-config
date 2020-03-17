25906400f05c5c7feab39584ee06368ebdfdba3b
Dusty Mabe <dusty@dustymabe.com>
2020-03-17 10:31:46 -0400

    add nm/dbus stuff
    only missing part is the alias of dbus.service to dbus-broker.service

--- overlay.d/10coreuser/usr/lib/dracut/modules.d/40ignition-conf/module-setup.sh
+++ overlay.d/10coreuser/usr/lib/dracut/modules.d/40ignition-conf/module-setup.sh
@@ -9,4 +9,36 @@ depends() {
 install() {
     inst "$moddir/base.ign" \
         "/usr/lib/ignition/base.ign"
+
+    inst_multiple -o \
+        ip \
+        nmtui \
+        nmcli \
+        find \
+        teamd teamdctl
+
+    inst_multiple -o \
+        busctl \
+        dbus-broker \
+        dbus-broker-launch \
+        /usr/lib/systemd/system/dbus.socket \
+        /usr/sbin/NetworkManager \
+        $systemdsystemunitdir/dbus-org.freedesktop.nm-dispatcher.service \
+        $systemdsystemunitdir/NetworkManager.service \
+        $systemdsystemunitdir/dbus-broker.service \
+        $systemdsystemunitdir/dbus.socket \
+        /usr/libexec/nm-dhcp-helper \
+        /usr/libexec/nm-dispatcher \
+        /usr/libexec/nm-iface-helper \
+        /usr/libexec/nm-ifdown \
+        /usr/libexec/nm-ifup
+
+    # Copy in dbus policies and NetworkManager supporting libs
+    for x in /usr/lib/sysusers.d /usr/share/dbus-1 /usr/lib*/NetworkManager ; do
+        for d in $(find $x -type d); do
+            inst_dir $d
+            inst_multiple -o $d/*
+        done
+    done
+    install_unit dbus.socket            socket.target.wants
 }
